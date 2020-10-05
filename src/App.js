e70d92994b7a3b10865cbf51d3fbcc6c968014a3
kj <agassi77@gmail.com>
2020-10-05 16:00:45 +0900

    강의진행을 위해 클래스형 컴포넌트로 변경

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello, React!!!
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       Hello, React!!!
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       Hello, React!!!
+      </div>
+    )
+  }
 }
 
 export default App;
