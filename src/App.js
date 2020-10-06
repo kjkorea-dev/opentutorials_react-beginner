159b0d17a72c32e07e7179c79f31df5b936670ad
kj <agassi77@gmail.com>
2020-10-06 14:23:21 +0900

    key

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      subject: { title: 'web', sub: 'World Wide Web!' }
+      subject: { title: 'WEB', sub: 'World Wide Web!' },
+      contents: [
+        {id: 1, title: 'HTML', desc: 'HTML is for information'},
+        {id: 2, title: 'CSS', desc: 'CSS is for design'},
+        {id: 3, title: 'JavaScript', desc: 'JavaScript is for information'}
+      ]
     }
   }
 
@@ -18,8 +23,8 @@ class App extends Component {
         <Subject 
           title={ this.state.subject.title } 
           sub={ this.state.subject.sub }>
-        </Subject>;
-        <TOC></TOC>
+        </Subject>
+        <TOC data={ this.state.contents }></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     )
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 
 class TOC extends Component {
   render() {
+    var data = this.props.data;
+    var lists = data.map((list, i) => {
+      return <li key={i}><a href={"/content/" + i}>{list.title}</a></li>;
+    });
     return (
       <nav>
-          <ul>
-              <li><a href="1.html">HTML</a></li>
-              <li><a href="2.html">CSS</a></li>
-              <li><a href="3.html">JavaScript</a></li>
-          </ul>
+          {lists}
       </nav>
     );
   }
