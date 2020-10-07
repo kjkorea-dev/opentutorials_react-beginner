82b3d95d3464be4ff3c4a817285d119d4fd1b003
kj <agassi77@gmail.com>
2020-10-07 17:54:42 +0900

    CRUD : Create - shouldComponentUpdate

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 
 class TOC extends Component {
+  // 데이터 변화없이 렌더링되는 것을 방지
+  shouldComponentUpdate(newProps, newState) {
+    console.log('Rendering TOC : shouldComponentUpdate');
+    return this.props.data !== newProps.data;
+  }
+
   render() {
+    console.log('Rendering TOC');
     var data = this.props.data;
     var lists = data.map((list, i) => {
       return <li key={list.id}>
